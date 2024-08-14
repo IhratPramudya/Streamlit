ba010bc465125ad0aae8ffa6f82866588eec017d
ihrat pramudya <ihratpramudya>
2024-08-14 09:47:43 +0700

    add pertanyaan

---
+++ pages/Pertanyaan Sidang.py
@@ -0,0 +1,54 @@
+import streamlit as st
+
+st.header("""Prediksi Pertanyaan Sidang Skripsi""")
+
+st.write("1. Pertanyaan pertama")
+st.write("A. Mengapa kamu mengambil judul penelitian tersebut ?")
+st.write("Jawab :")
+st.write("- Karena kasus stunting Kab.Batang masih tinggi oleh karena itu kami\
+         melakukan pemetaan untuk membantu pemerintah agar mengetahui\
+         daerah mana yang perlu diprioritaskan.")
+st.write("- Karena sejauh ini belum ada yang melakukan penelitian mengenai\
+         klasterisasi tingkat kasus stunting di Kabupaten Batang")
+st.write("B. Apa definisi konsep utama dalam penelitian (yang ada di judul) ?")
+st.write("Jawab:")
+st.write("- Implementasi: Menurut KBBI implementasi yaitu\
+         pelaksanaan/penerapan. Dimana arti umum, implementasi merupakan\
+         sebuah pelaksanaan/penerapan sebuah rencana untuk mencapai tujuan\
+         tertentu")
+st.write("- Algoritma : Menurut KBBI, algoritma adalah suatu prosedur sistematis\
+         untuk menyelesaikan masalah matematika dalam langkah-langkah\
+         terbatas atau urutan pengambilan keputusan yang logis untuk\
+         memecahkan masalah tersebut.")
+st.write("- Klasterisasi : Adalah pembentukan sesuatu menjadi kluster.\
+         Pengelompokkan sesuatu berdasarkan kesamaan, fungsi dan\
+         sebagainya.")
+st.write("- Stunting : Stunting berasal dari Bahasa inggris yang berarti\
+         pengerdilan. Sementara arti kerdil menurut KBBI selalu kecil saja, tidak\
+         dapat menjadi besar (tentang orang, binatang, tumbuhan, dan\
+         sebagainya) karena kekurangan gizi atau karena keturunan.")
+st.write("2. Kenapa penelitian kamu mengambil topik stunting itu ?")
+st.write("Jawab :")
+st.write("- Karena presentase kasus stunting di Indonesia masih terbilang cukup tinggi.\
+         Berdasarkan Survei Status Gizi Indonesia (SSGI) oleh Kementerian\
+         Kesehatah Republik Indonesia tahun 2022 presentase kasus stunting di\
+         Indonesia adalah 21,6. (website resmi Kemenkes 'Standard WHO terkait \
+         prevalensi stunting harus angka kurang dari 20%').")
+st.write("3. Kenapa memilih lokasi tersebut ?")
+st.write("Jawab :")
+st.write("- Karena presentase kasus stunting di Kabupaten Batang tergolong tinggi\
+         dengan angka 23,5 persen pada tahun 2022 (SSGI 2022 survei status gizi\
+         indonesia). (website resmi Kemenkes 'Standard WHO terkait prevalensi\
+         stunting harus di angka kurang dari 20%')")
+st.write("4. Penelitian kamu ini termasuk penelitian jenis apa ?")
+st.write("Jawab:")
+st.write("- Penelitian kami termasuk ke penelitian kuantitatif karena penelitian kami\
+         menggunakan data dalam bentuk angka, yang dapat diuji secara statistik.\
+         (Pengertian statistik : data berupa angka yang dapat dianalisis untuk\
+         mendapatkan informasi dari suatu masalah).")
+st.write("5. Manfaat dari penelitian kalian ini apa ?")
+st.write("6. Seberapa penting penelitian kalian ini untuk direalisasikan ?")
+st.write("Jawab no 5 & 6 :")
+st.write("Untuk membantu pemerintahaan dalam merumuskan kebijakan yang lebih\
+         efektif tingkat keparahan kasus dan untuk mengetahui skala prioritas untuk\
+         penanganan dan pencegahan kasus stunting di Kabupaten Batang.")
