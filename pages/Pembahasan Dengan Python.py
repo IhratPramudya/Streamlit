9f0ac4b302bf5edced2f7b7e02ca4ea76f8cd974
ihrat pramudya <ihratpramudya>
2024-08-18 14:39:04 +0700

    Add code

--- pages/Pembahasan Dengan Python.py
+++ pages/Pembahasan Dengan Python.py
@@ -148,6 +148,8 @@ code = '''
 
 st.code(code, language='python')
 
+st.subheader("Penjelasan kode di atas")
+
 st.write("Kode tersebut digunakan untuk membuat dan menampilkan scatter plot (grafik sebar) dari data yang telah dibersihkan dari outlier. Berikut penjelasan ilmiahnya:")
 
 st.write("plt.figure(figsize=[12, 8]):")
@@ -173,6 +175,19 @@ code = '''
 
 st.code(code, language='python')
 
+st.subheader("Penjelasan kode di atas")
+st.write("1. Membuat Salinan Data:")
+st.write("stunting_process = new_stunting.copy(): Kode ini membuat salinan dari DataFrame new_stunting dan menyimpannya dalam variabel baru bernama stunting_process.\
+Mengapa Disalin?: Dengan membuat salinan, Anda bisa melakukan berbagai pengolahan atau perubahan pada stunting_process tanpa mengubah data asli di new_stunting. Ini berguna agar data asli tetap aman dan tidak terpengaruh oleh perubahan yang Anda lakukan.")
+
+st.write("2. Menampilkan Data:")
+
+st.write("stunting_process: Ketika Anda memanggil variabel ini, data yang ada di dalam stunting_process akan ditampilkan. Anda bisa melihat isinya untuk memastikan data sudah siap untuk diolah lebih lanjut.")
+st.write("3. Manfaatnya:")
+st.write("Keamanan Data: Data asli tetap utuh dan tidak berubah.\
+Fleksibilitas: Anda bebas melakukan modifikasi pada salinan data untuk keperluan analisis atau pengolahan lebih lanjut.")
+
+
 
 code = '''
     #drop kolom yang tidak digunakan
@@ -182,6 +197,30 @@ code = '''
 '''
 
 st.code(code, language='python')
+st.subheader("Penjelasan kode di atas")
+st.write("1. Menentukan Kolom yang Akan Dihapus:")
+st.write("- drop_column = ['puskesmas', 'desa_kelurahan', 'persen']:")
+
+st.write("- Anda membuat daftar berisi nama-nama kolom yang ingin dihapus dari DataFrame. Dalam hal ini, kolom puskesmas, desa_kelurahan, dan persen dipilih untuk dihapus karena mungkin tidak diperlukan untuk analisis lebih lanjut.")
+
+st.write("2. Menghapus Kolom dari DataFrame:")
+st.write("- stunting_process.drop(drop_column, inplace=True, axis=1):")
+st.write("- drop(): Fungsi ini digunakan untuk menghapus baris atau kolom dari DataFrame.")
+st.write("- drop_column: Ini adalah daftar kolom yang Anda tentukan sebelumnya untuk dihapus.")
+st.write("- inplace=True: Argumen ini memastikan bahwa perubahan dilakukan langsung pada DataFrame stunting_process, tanpa perlu membuat salinan baru. Jika inplace=False, DataFrame asli tidak akan berubah, dan Anda perlu menyimpan hasilnya ke dalam variabel baru.")
+
+st.write("- axis=1: Ini menunjukkan bahwa Anda ingin menghapus kolom (bukan baris). Dalam Pandas, axis=0 berarti baris, dan axis=1 berarti kolom.")
+
+st.write("3. Menampilkan DataFrame yang Sudah Diperbarui:")
+
+st.write("- stunting_process: Setelah penghapusan, Anda memanggil variabel ini untuk menampilkan DataFrame stunting_process yang sekarang tidak lagi memiliki kolom puskesmas, desa_kelurahan, dan persen.")
+
+st.write("4. Tujuan Penghapusan Kolom:")
+
+st.write("- Efisiensi: Menghapus kolom yang tidak relevan dapat membuat DataFrame lebih ringkas dan fokus pada data yang penting untuk analisis.")
+st.write("- Kebersihan Data: Dengan menghapus kolom yang tidak digunakan, DataFrame menjadi lebih mudah diinterpretasikan dan diolah lebih lanjut.")
+
+
 
 
 code = '''
@@ -195,6 +234,37 @@ code = '''
 '''
 
 st.code(code, language='python')
+st.subheader("Penjelasan kode di atas")
+
+st.write("1. Inisialisasi Daftar untuk Menyimpan SSE:")
+st.write("- sse_dist = []: Ini adalah daftar kosong yang akan digunakan untuk menyimpan nilai Sum of Square Error (SSE) untuk setiap jumlah klaster yang diuji.")
+
+st.write("2. Loop untuk Menghitung SSE dengan Berbagai Jumlah Klaster:")
+
+st.write("- for i in range(1, 10):\
+Ini adalah loop yang akan berjalan dari i = 1 hingga i = 9. Setiap iterasi loop ini akan menjalankan proses K-Means dengan jumlah klaster (n_clusters) yang berbeda, mulai dari 1 hingga 9.")
+
+st.write("3. Membangun Model K-Means dan Melatihnya:")
+
+st.write("- model = KMeans(n_clusters=i, n_init='auto'):")
+
+st.write("KMeans(n_clusters=i, n_init='auto'): Di sini, Anda membuat objek model K-Means dari sklearn.cluster.KMeans, dengan jumlah klaster (n_clusters) yang diatur berdasarkan nilai i dalam loop.")
+
+st.write("- n_clusters=i: Menentukan jumlah klaster yang akan dibentuk oleh model.")
+st.write("- n_init='auto': Parameter ini mengatur berapa kali algoritma K-Means dijalankan dengan inisialisasi yang berbeda-beda untuk memastikan solusi terbaik ditemukan. Versi 'auto' akan memilih pengaturan yang paling sesuai secara otomatis.")
+st.write("- model.fit(stunting_process):")
+st.write("- fit(stunting_process): Melatih model K-Means menggunakan data stunting_process. Proses ini melibatkan penentuan posisi centroid (pusat klaster) untuk setiap klaster yang diminta.")
+st.write("4. Menghitung dan Menyimpan SSE:")
+st.write("- sse_dist.append(model.inertia_):\
+model.inertia_: Setelah model K-Means dilatih, inertia_ adalah metrik yang mengukur total Sum of Square Error (SSE), yaitu jumlah jarak kuadrat antara titik data dan centroid terdekat. Nilai ini disimpan ke dalam daftar sse_dist.")
+st.write("- Mengapa SSE penting?: SSE mengukur seberapa baik data telah dikelompokkan ke dalam klaster. Nilai SSE yang lebih rendah menunjukkan klaster yang lebih kompak dan lebih baik dalam mengelompokkan data.")
+st.write("5. Menampilkan Hasil SSE untuk Setiap Klaster:")
+st.write("print('Sum of square error for {} clusters: {}''.format(i, sse_dist[i-1])):")
+st.write("- format(i, sse_dist[i-1]): Ini adalah pernyataan format yang akan mencetak jumlah klaster saat ini (i) dan nilai SSE yang terkait (sse_dist[i-1]).")
+st.write("Mengapa Dicetak?: Mencetak SSE untuk setiap jumlah klaster membantu Anda melihat bagaimana SSE berubah saat jumlah klaster bertambah. Ini berguna untuk menentukan jumlah klaster yang optimal.")
+st.write("6. Tujuan Analisis:")
+st.write("Menentukan Jumlah Klaster Optimal: Salah satu cara umum untuk memilih jumlah klaster yang tepat adalah dengan menggunakan Metode Elbow. Dalam metode ini, Anda membuat grafik jumlah klaster terhadap SSE, dan mencari 'elbow' (titik di mana penurunan SSE mulai melambat). Jumlah klaster pada titik ini sering dipilih sebagai jumlah klaster optimal karena menyeimbangkan antara kompleksitas model dan representasi data yang baik.")
+
 
 code = '''
     #membuat plot SSE
@@ -214,6 +284,46 @@ code = '''
 st.code(code, language='python')
 
 
+st.subheader("1. Penjelasan kode di atas")
+st.write("- sse = {}:")
+
+st.write("sse adalah dictionary kosong yang akan digunakan untuk menyimpan nilai SSE untuk setiap jumlah klaster (dengan jumlah klaster sebagai kunci dan nilai SSE sebagai nilai).")
+
+st.write("2. Loop untuk Menghitung SSE dengan Berbagai Jumlah Klaster:")
+
+st.write("- for k in range(1, 10):")
+
+st.write("Loop ini berjalan dari k = 1 hingga k = 9. Setiap iterasi akan menghitung SSE untuk model K-Means dengan jumlah klaster k.")
+
+st.write("3. Membangun dan Melatih Model K-Means:")
+
+st.write("- model = KMeans(n_clusters=k, n_init='auto').fit(stunting_process):")
+
+st.write("-KMeans(n_clusters=k, n_init='auto'): Membuat model K-Means dengan k klaster.")
+
+st.write("- fit(stunting_process): Melatih model menggunakan data stunting_process.")
+st.write("- sse[k] = model.inertia_:")
+st.write("- model.inertia_: Setelah model dilatih, nilai inertia (SSE) disimpan dalam dictionary sse dengan kunci k. Inertia mengukur jumlah jarak kuadrat antara data dan klaster terdekat.")
+st.write("4. Membuat dan Mengatur Plot:")
+st.write("- plt.figure(): Ini memulai pembuatan plot baru.")
+st.write("- plt.plot(list(sse.keys()), list(sse.values())):")
+st.write("- list(sse.keys()): Mengambil jumlah klaster dari dictionary sse sebagai sumbu-x.")
+st.write("- list(sse.values()): Mengambil nilai SSE yang sesuai sebagai sumbu-y.")
+st.write("Plot ini menggambarkan bagaimana SSE berubah dengan jumlah klaster.\
+plt.xlabel('Number of cluster''):")
+st.write("- Menambahkan label 'Number of cluster' pada sumbu-x.\
+plt.ylabel('SSE'):")
+st.write("- Menambahkan label 'SSE' pada sumbu-y.\
+plt.title('Plot Sum of Square Error'):")
+st.write("- Memberikan judul pada plot: 'Plot Sum of Square Error''.\
+plt.show():")
+st.write("- Menampilkan plot yang telah dibuat.")
+st.write("5. Tujuan dan Manfaat:")
+st.write("Metode Elbow: Tujuan dari plot ini adalah untuk membantu Anda menggunakan Metode Elbow untuk menentukan jumlah klaster yang optimal. Dalam plot, Anda mencari titik di mana penurunan SSE mulai melambat (membentuk 'elbow'). Jumlah klaster pada titik ini sering dipilih sebagai jumlah klaster optimal, karena menyeimbangkan antara kompleksitas model dan representasi data yang baik.")
+
+
+
+
 code = '''
     #mencari silhouette score
     silhouette_scores = []
@@ -225,6 +335,52 @@ code = '''
 '''
 
 st.code(code, language='python')
+
+st.subheader("Penjelasan kode di atas")
+
+st.write("1. Inisialisasi Daftar untuk Menyimpan Silhouette Score:")
+st.write("- silhouette_scores = []: \
+         Daftar kosong ini digunakan untuk menyimpan nilai Silhouette Score untuk setiap jumlah klaster yang diuji.")
+
+
+
+st.write("2. Loop untuk Menghitung Silhouette Score dengan Berbagai Jumlah Klaster:")
+
+st.write("- for i in range(2, 11): \
+         Loop ini berjalan dari i = 2 hingga i = 10. Setiap iterasi menghitung Silhouette Score untuk model K-Means dengan jumlah klaster i.")
+
+st.write("- Mengapa mulai dari 2?: Silhouette Score memerlukan minimal dua klaster untuk dihitung, karena metrik ini mengukur seberapa mirip titik data dengan klaster mereka sendiri dibandingkan dengan klaster lain.")
+
+st.write("3. Membangun dan Melatih Model K-Means:")
+
+st.write("- model = KMeans(n_clusters = i, n_init='auto'):")
+
+st.write("- KMeans(n_clusters = i, n_init='auto'): Membuat model K-Means dengan i klaster.")
+
+st.write("- model.fit(stunting_process): Melatih model K-Means menggunakan data stunting_process. Setelah model dilatih, setiap titik data akan dialokasikan ke salah satu klaster yang dihasilkan oleh model.")
+
+st.write("4. Menghitung dan Menyimpan Silhouette Score:")
+
+st.write("- silhouette_scores.append(silhouette_score(stunting_process, model.labels_)):")
+
+st.write("- silhouette_score(stunting_process, model.labels_): Fungsi ini menghitung Silhouette Score untuk data stunting_process berdasarkan label klaster (model.labels_) yang dihasilkan oleh model K-Means.")
+
+st.write("Apa itu Silhouette Score?: Nilai Silhouette Score berkisar antara -1 dan 1. Nilai mendekati 1 menunjukkan bahwa titik-titik data dikelompokkan dengan baik (klaster yang padat dan terpisah dengan jelas). Nilai mendekati 0 menunjukkan bahwa titik-titik data berada di batas antara dua klaster, dan nilai mendekati -1 menunjukkan bahwa titik-titik data mungkin telah dialokasikan ke klaster yang salah.\
+Nilai Silhouette Score yang dihitung kemudian ditambahkan ke daftar silhouette_scores.")
+
+st.write("5. Menampilkan Hasil Silhouette Score:")
+
+st.write("print('Silhouette Score for {} clusters: {}'.format(i, silhouette_scores[i-2])):")
+
+st.write("- format(i, silhouette_scores[i-2]): Pernyataan ini digunakan untuk mencetak jumlah klaster saat ini (i) dan nilai Silhouette Score yang sesuai (silhouette_scores[i-2]).")
+
+st.write("- i-2: Karena indeks di Python dimulai dari 0, dan loop dimulai dari 2, maka untuk mengambil nilai dari daftar silhouette_scores, Anda menggunakan indeks i-2.")
+
+st.write("6. Tujuan Analisis:")
+
+st.write("- Evaluasi Klasterisasi: Silhouette Score membantu Anda mengevaluasi seberapa baik klasterisasi yang dilakukan oleh model K-Means. Anda dapat membandingkan nilai Silhouette Score untuk berbagai jumlah klaster untuk menemukan jumlah klaster yang memberikan nilai tertinggi, yang sering dianggap sebagai jumlah klaster yang optimal.")
+
+
 
 code = '''
     #membuat plot silhouette score
@@ -241,6 +397,31 @@ code = '''
 '''
 
 st.code(code, language='python')
+st.write("1. Mengatur Ukuran Plot:")
+st.write("- plt.figure(figsize=[12, 8]):\
+         Ini mengatur ukuran figur plot menggunakan Matplotlib. Meskipun tidak diperlukan untuk Plotly Express, ini bisa menjadi langkah tambahan jika Anda menggunakan Matplotlib dalam konteks lain. Untuk Plotly Express, ukuran plot diatur dalam parameter fig.update_layout().")
+st.write("2. Membuat Plot dengan Plotly Express:")
+st.write("- fig = px.line(x=range(2, 11), y=silhouette_scores):")
+st.write("- px.line(): Fungsi dari Plotly Express untuk membuat plot garis.")
+st.write("- x=range(2, 11): Menentukan sumbu-x plot, yang menunjukkan jumlah klaster (dari 2 hingga 10).")
+st.write("- y=silhouette_scores: Menentukan sumbu-y plot, yang berisi nilai Silhouette Score yang telah dihitung sebelumnya.")
+st.write("3. Mengatur Tata Letak Plot:")
+st.write("- fig.update_layout():")
+st.write("- xaxis = dict(tickvals = list(range(2, 11))):\
+         Mengatur nilai-nilai pada sumbu-x (jumlah klaster) untuk ditampilkan pada plot.")
+st.write("- title='Plot Silhouette Score':\
+         Memberikan judul pada plot: 'Plot Silhouette Score'.")
+st.write("- xaxis_title='Number of Clusters':\
+         Menambahkan label 'Number of Clusters' pada sumbu-x.")
+st.write("- yaxis_title='Silhouette Score':\
+         Menambahkan label 'Silhouette Score' pada sumbu-y.")
+st.write("4. Menampilkan Plot:")
+st.write("- fig.show():")
+
+st.write("Menampilkan plot interaktif yang telah dibuat. Ini memungkinkan Anda untuk berinteraksi dengan plot, seperti melakukan zoom in/out atau melihat nilai-nilai secara detail.")
+
+
+
 code = '''
     #membuat silhouette score visualizer
     model = KMeans(n_clusters=2, n_init="auto")
